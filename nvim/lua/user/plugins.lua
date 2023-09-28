a5c5a4c1fd1f3300084bdd77a115a923d6e84692
Toplica Tanasković <toplica.tanaskovic@tn-tech.co.rs>
2023-09-28 08:59:27 +0200

    Added lualine and whichkey

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -44,6 +44,8 @@ return packer.startup(function(use)
     use "wbthomason/packer.nvim" -- Have packer manage itself
     use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
     use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
+    use "folke/which-key.nvim"
+    use "nvim-lualine/lualine.nvim"
 
     -- Color schemes
     use "folke/tokyonight.nvim" 
