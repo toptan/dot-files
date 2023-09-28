961cd17163564a7cfdf1df98dfe5cc3c4a14f22b
Toplica Tanasković <toplica.tanaskovic@tn-tech.co.rs>
2023-09-28 11:25:36 +0200

    Added indent line

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -48,6 +48,7 @@ return packer.startup(function(use)
     use "nvim-lualine/lualine.nvim"
     use { "kyazdani42/nvim-web-devicons", commit = "563f3635c2d8a7be7933b9e547f7c178ba0d4352" }
     use { "kyazdani42/nvim-tree.lua", commit = "7282f7de8aedf861fe0162a559fc2b214383c51c" }
+    use { "lukas-reineke/indent-blankline.nvim", commit = "db7cbcb40cc00fc5d6074d7569fb37197705e7f6" }
 
     -- Color schemes
     use "folke/tokyonight.nvim" 
