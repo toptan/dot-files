c75f28a30612a570df30f94f95375adb106424b4
Toplica Tanasković <toplica.tanaskovic@tn-tech.co.rs>
2023-10-10 09:09:03 +0200

    Resolving conflicts

--- nvim/lua/user/colorscheme.lua
+++ nvim/lua/user/colorscheme.lua
@@ -1,5 +1,4 @@
--- local colorscheme = "tokyonight-night"
-local colorscheme = "NeoSolarized"
+local colorscheme = "catppuccin"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
