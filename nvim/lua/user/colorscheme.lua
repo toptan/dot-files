0b6c037a4babc34cb55d917304c7ee36113dd4b3
Toplica Tanasković <toplica.tanaskovic@tn-tech.co.rs>
2023-11-15 11:33:55 +0100

    Fixing everforest colorscheme

--- nvim/lua/user/colorscheme.lua
+++ nvim/lua/user/colorscheme.lua
@@ -1,5 +1,10 @@
--- local colorscheme = "everforest"
-local colorscheme = "tokyonight-night"
+local colorscheme = "everforest"
+-- local colorscheme = "tokyonight-night"
+
+if colorscheme == "everforest" then
+    vim.g.everforest_background = "hard"
+    vim.g.everforest_better_performance = 1
+end
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
