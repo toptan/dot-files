2f225337e1df2324445261f8e53e8eefcce9d5e9
Toplica Tanasković <toplica.tanaskovic@tn-tech.co.rs>
2023-11-15 11:24:45 +0100

    Adding switch header/source for clangd

--- nvim/lua/user/lsp/handlers.lua
+++ nvim/lua/user/lsp/handlers.lua
@@ -80,6 +80,10 @@ M.on_attach = function(client, bufnr)
         client.server_capabilities.documentFormattingProvider = false
     end
 
+    if client.name == "clangd" then
+        vim.api.nvim_buf_set_keymap(bufnr, "n", "<F3>", "<cmd>ClangdSwitchSourceHeader<cr>", { noremap = true, silent = true })
+    end
+
     lsp_keymaps(bufnr)
     local status_ok, illuminate = pcall(require, "illuminate")
     if not status_ok then
